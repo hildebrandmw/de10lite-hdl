58bc103730f2f329fef2331fd71f13680d19b0d7
Mark <mhildebrand@ucdavis.edu>
2018-02-18 09:51:10 -0800

    Removed horrid camel-case.

--- uart/hdl/uart_handler.v
+++ uart/hdl/uart_handler.v
@@ -7,13 +7,13 @@ module uart_handler
 
    // RS232 Signals
    output wire UART_TX,
-   input wire UART_RX,
+   input  wire UART_RX,
 
    // Interfacing Signals
-   input  wire [ADDR_BITS-1:0] addr,
-   input  wire writeEnable,
-   input  wire [7:0] dataIn,
-   output wire [7:0] dataOut,
+   input  wire [ADDR_BITS-1:0]  addr,
+   input  wire                  write_enable,
+   input  wire [7:0]            data_in,
+   output wire [7:0]            data_out,
 
    // Start and Ready Signals
    input  wire start,
@@ -30,6 +30,7 @@ module uart_handler
 
    reg [7:0] mem [RAM_SIZE-1:0] /* synthesis ramstyle = "M4K" */;
 
+   // Specify initial contents to zero.
    initial begin : RAM_INIT
       integer i;
       for (i = 0; i < RAM_SIZE; i = i+1) begin
@@ -38,16 +39,20 @@ module uart_handler
    end
 
    // Data
-   reg [7:0] ramData;
+   reg [7:0] ram_data;
 
-   reg [ADDR_BITS-1:0] addrInternal;
-   reg [ADDR_BITS-1:0] addrInternalNext;
-   wire [ADDR_BITS-1:0] addrActual;
+   // Separate signals for
+   //   - externally supplied
+   //   - internally supplied
+   //   - actual (multiplexed between external and internal)
+   reg  [ADDR_BITS-1:0] addr_internal;
+   reg  [ADDR_BITS-1:0] addr_internal_next;
+   wire [ADDR_BITS-1:0] addr_actual;
 
-   reg writeEnableInternal;
-   wire writeEnableActual;
+   reg  write_en_internal;
+   wire write_en_actual;
 
-   wire [7:0] ramDataInActual;
+   wire [7:0] ram_data_in_actual;
 
    //---------------//
    // State Machine //
@@ -64,10 +69,10 @@ module uart_handler
    localparam RESET_STATE = PREPARE_TO_RECEIVE;
 
    // State Signals
-   reg [2:0] state, nextState;
+   reg [2:0] state, next_state;
 
    // Output Signals
-   reg enableExternalAddressing;
+   reg en_external_addressing;
 
    //----------------//
    // UART Interface //
@@ -75,14 +80,14 @@ module uart_handler
 
    // From UART
    //  wire fromUartReady;
-   wire [7:0] fromUartData;
+   wire [7:0] from_uart_data;
    wire fromUartError;
-   wire fromUartValid;
+   wire from_uart_valid;
 
    // To UART
-   wire toUartReady;
-   wire [7:0] toUartData;
-   reg toUartValid;
+   wire       to_uart_ready;
+   wire [7:0] to_uart_data;
+   reg        to_uart_valid;
    // wire toUartError;
 
 
@@ -98,24 +103,22 @@ module uart_handler
    While this module is receiving or transmitting data from the UART module,
    it has total control over this RAM module.
 
-   Otherwise, "enableExternalAddressing" is asserted and an external module
+   Otherwise, "en_external_addressing" is asserted and an external module
    has control.
    */
 
-   assign addrActual        = enableExternalAddressing ? addr        : addrInternal;
-   assign writeEnableActual = enableExternalAddressing ? writeEnable : writeEnableInternal;
-   assign ramDataInActual   = enableExternalAddressing ? dataIn      : fromUartData;
+   assign addr_actual        = en_external_addressing ? addr         : addr_internal;
+   assign write_en_actual    = en_external_addressing ? write_enable : write_en_internal;
+   assign ram_data_in_actual = en_external_addressing ? data_in      : from_uart_data;
 
-   // RAM READ
-   //assign ramData = mem[addrActual];
-   assign dataOut = ramData;
-   assign toUartData = ramData;
+   assign data_out      = ram_data;
+   assign to_uart_data  = ram_data;
 
-   // RAM WRITE
+   // RAM READ/WRITE
    always @(posedge clk) begin
-      ramData <= mem[addrActual];
-      if (writeEnableActual) begin
-         mem[addrActual] <= ramDataInActual;
+      ram_data <= mem[addr_actual];
+      if (write_en_actual) begin
+         mem[addr_actual] <= ram_data_in_actual;
       end
    end
 
@@ -126,19 +129,19 @@ module uart_handler
    // Instantiation
    uart u0 (
       // Global Signals
-      .clk(clk),
+      .clk  (clk),
       .reset(reset),
-      .tx(UART_TX),
-      .rx(UART_RX),
+      .tx   (UART_TX),
+      .rx   (UART_RX),
 
       // From Uart
-      .rx_data(fromUartData),
-      .rx_valid(fromUartValid),
+      .rx_data  (from_uart_data),
+      .rx_valid (from_uart_valid),
 
       // To Uart
-      .tx_ready(toUartReady),
-      .tx_data(toUartData),
-      .tx_transmit(toUartValid)
+      .tx_ready     (to_uart_ready),
+      .tx_data      (to_uart_data),
+      .tx_transmit  (to_uart_valid)
    );
 
    //---------------//
@@ -152,42 +155,41 @@ module uart_handler
       if (reset) begin
          state <= RESET_STATE;
       end else begin
-         state <= nextState;
+         state <= next_state;
       end
 
       // Addressing Update
-      addrInternal <= addrInternalNext;
+      addr_internal <= addr_internal_next;
    end
 
    // Next State Logic
    always @(*) begin
       // Default Next State (avoid inferring latches)
-      nextState = state;
-      addrInternalNext = addrInternal;
+      next_state         = state;
+      addr_internal_next = addr_internal;
 
       // Default Output Values
-
-      writeEnableInternal = 0;      // Local RAM Write Enable
-      enableExternalAddressing = 0; // Hand RAM control to external module if 1
-      toUartValid = 0;              // Valid Data if 1
-      ready = 0;
+      write_en_internal      = 0; // Local RAM Write Enable
+      en_external_addressing = 0; // Hand RAM control to external module if 1
+      to_uart_valid          = 0; // Valid Data if 1
+      ready                  = 0;
 
       case(state)
 
          // Initialize local components if necessary
          PREPARE_TO_RECEIVE: begin
-            addrInternalNext = 0;
-            nextState = RECEIVING_UART;
+            addr_internal_next = 0;
+            next_state = RECEIVING_UART;
          end
 
          // Store Data when it arrives.
          // Exit when expected amount of data received
          RECEIVING_UART: begin
-            if (fromUartValid) begin
-               addrInternalNext = addrInternal + 1;
-               writeEnableInternal = 1;
-               if (addrInternal == RAM_SIZE - 1) begin
-                  nextState = IDLE;
+            if (from_uart_valid) begin
+               addr_internal_next = addr_internal + 1;
+               write_en_internal = 1;
+               if (addr_internal == RAM_SIZE - 1) begin
+                  next_state = IDLE;
                end
             end
          end
@@ -195,36 +197,37 @@ module uart_handler
          // Hand Control to external Module.
          // Wait for assertion of "start"
          IDLE : begin
-            enableExternalAddressing = 1;
+            en_external_addressing = 1;
             ready = 1;
             if (start) begin
-               nextState = PREPARE_TO_TRANSMIT;
+               next_state = PREPARE_TO_TRANSMIT;
             end
          end
 
          // Initialize local components if necessary
          PREPARE_TO_TRANSMIT : begin
-            addrInternalNext = 0;   // Reset local addresser
-            nextState = TRANSMIT_WAIT_FOR_READY;
+            addr_internal_next = 0;   // Reset local addresser
+            next_state = TRANSMIT_WAIT_FOR_READY;
          end
 
-         // Stall until UART is ready to transmit
+         // Stall until UART is ready to transmit the next byte
          TRANSMIT_WAIT_FOR_READY : begin
-            if (toUartReady) begin
-               nextState = TRANSMIT_DATA;
+            if (to_uart_ready) begin
+               next_state = TRANSMIT_DATA;
             end
          end
 
-         // Transmit One Data Packet
+         // Transmit One Byte of data.
+         // Repeat for each entry in the the RAM.
          TRANSMIT_DATA : begin
-            toUartValid = 1;
-            addrInternalNext = addrInternal + 1;
+            to_uart_valid = 1;
+            addr_internal_next = addr_internal + 1;
 
             // Test is last RAM entry is written
-            if (addrInternal == RAM_SIZE - 1) begin
-               nextState = PREPARE_TO_RECEIVE;
+            if (addr_internal == RAM_SIZE - 1) begin
+               next_state = PREPARE_TO_RECEIVE;
             end else begin
-               nextState = TRANSMIT_WAIT_FOR_READY;
+               next_state = TRANSMIT_WAIT_FOR_READY;
             end
          end
          default;
